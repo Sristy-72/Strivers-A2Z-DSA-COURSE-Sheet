90cdd2e57da0e4a3d423868600bcf75b5072a1d2
Sristy <126855796+Sristy-72@users.noreply.github.com>
2024-06-07 17:23:26 +0530

    Create 268.Missing_number(leetcode).cpp

---
+++ 268.Missing_number(leetcode).cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        vector<int>ans;
+        int n=nums.size();
+        int sum= (n*(n+1)/2);
+        //for(int i=0;i<n;i++){
+           // int sum1=sum+nums[i];
+       // }
+       return sum-accumulate(nums.begin(),nums.end(),0);
+       // accumulate is used to iterate over a sequence of elements
+       // accumulate(starting range, ending range,intial value)
+    }
+};
