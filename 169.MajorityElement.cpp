48c19295956bb05283abe1284c8872d88d34cb07
Sristy <126855796+Sristy-72@users.noreply.github.com>
2024-06-16 12:57:24 +0530

    Update 169.MajorityElement.cpp

--- 169.MajorityElement.cpp
+++ 169.MajorityElement.cpp
@@ -1,14 +1,39 @@
 #include <bits/stdc++.h>
 int findMajorityElement(int arr[], int n) {
   // Better approach
-	map<int,int>mpp;
+	// map<int,int>mpp;
+	// for(int i=0;i<n;i++){
+	// 	mpp[arr[i]]++;
+	// }
+	// for(auto it:mpp){
+	// 	if(it.second>n/2){
+	// 		return it.first;
+	// 	}
+	// }
+	// return -1;
+	//optimal approach using Moore's voting algorithm
+	int maj=arr[0];
+	int cnt=0;
 	for(int i=0;i<n;i++){
-		mpp[arr[i]]++;
+		if(cnt==0){
+			maj=arr[i];
+           cnt=1;
+		}
+		else if(maj==arr[i]){
+			cnt++;
+		}
+		else
+		cnt--;
 	}
-	for(auto it:mpp){
-		if(it.second>n/2){
-			return it.first;
+	int cnt1=0;
+	for(int i=0;i<n;i++){
+		if(arr[i]==maj){
+			cnt1++;
+		}
+		if(cnt1>(n/2)){
+			return maj;
 		}
 	}
 	return -1;
+	// return maj;
 }
