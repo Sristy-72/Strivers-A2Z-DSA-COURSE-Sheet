2c1f2286f84db1cb234be8331001c85e863af0a2
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-03-15 23:44:41 +0530

    Update Nth fibonacci Number.cpp

--- DP/Nth fibonacci Number.cpp
+++ DP/Nth fibonacci Number.cpp
@@ -8,18 +8,36 @@
 //SOLUTION
 #include<bits/stdc++.h>
 using namespace std;
+
 int fib(int n,vector<int>&dp){
-        
-        if(n<=1) return n;
-        if(dp[n]!=-1) return dp[n];
-        dp[n]=fib(n-1,dp)+fib(n-2,dp);
-        return dp[n];
+        // memoization
+        // if(n<=1) return n;
+        // if(dp[n]!=-1) return dp[n];
+        // dp[n]=fib(n-1,dp)+fib(n-2,dp);
+        // return dp[n];
+
+   // TABULATION
+    dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+                dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];   
 }
 int main()
 {
         int n;
         cin>>n;
        vector<int>dp(n+1,-1);
-       cout<< fib(n,dp)<<endl;
+   // SPACE OPTIMIZATION
+     int prev=1;
+        int prev2=0;
+        for(int i=2;i<=n;i++){
+                int curri= prev2+ prev;
+                  prev2=prev;
+                prev=curri;        
+        }
+        cout<<prev<<endl;
+       //cout<< fib(n,dp)<<endl;
        return 0;
 }
