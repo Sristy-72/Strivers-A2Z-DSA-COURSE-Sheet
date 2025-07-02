8511189802baf3768cf8d03a3e40e9eec1cdf0bf
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-07-02 16:59:11 +0530

    Create First_negative_integer_window.cpp

---
+++ Sliding Window/First_negative_integer_window.cpp
@@ -0,0 +1,29 @@
+//Given an array arr[]  and a positive integer k, find the first negative integer for each and every window(contiguous subarray) of size k.
+//Note: If a window does not contain a negative integer, then return 0 for that window.
+//Input: arr[] = [-8, 2, 3, -6, 10] , k = 2
+//Output: [-8, 0, -6, -6]
+
+
+class Solution {
+  public:
+    vector<int> firstNegInt(vector<int>& arr, int k) {
+        // write code here
+        int n= arr.size();
+        int first_neg=0;
+        vector<int>result;
+       queue<int>q;
+       for(int i=0;i<n;i++){
+           if(arr[i]<0)
+           q.push(i);
+           if(!q.empty() && q.front()<=i-k){
+               q.pop();
+           }
+           if(i>=k-1){
+               
+              if(!q.empty()) result.push_back(arr[q.front()]);
+              else result.push_back(0);
+           }
+       }
+        return result;
+    }
+};
