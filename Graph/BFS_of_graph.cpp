3aa848148752409ad3e76a98fa856a6fcc0af097
Sristy <126855796+Sristy-72@users.noreply.github.com>
2025-09-09 22:47:39 +0530

    Create BFS_of_graph.cpp

---
+++ Graph/BFS_of_graph.cpp
@@ -0,0 +1,36 @@
+//Given a connected undirected graph containing V vertices, represented by a 2-d adjacency list adj[][], 
+//where each adj[i] represents the list of vertices connected to vertex i. Perform a Breadth First Search (BFS)
+//traversal starting from vertex 0, visiting vertices from left to right according to the given adjacency list, and return a list containing the BFS traversal of the graph.
+
+//Note: Do traverse in the same order as they are in the given adjacency list.
+
+//Examples:
+
+//Input: adj[][] = [[2, 3, 1], [0], [0, 4], [0], [2]]
+
+//Output: [0, 2, 3, 1, 4]
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfs(vector<vector<int>> &adj) {
+        // Code here
+        int n= adj.size();
+        vector<int>bfs;
+        queue<int>q;
+        int vis[n]={0};
+        vis[0]=1;
+        q.push(0);
+        while(!q.empty()){
+            int node= q.front();
+            q.pop();
+            bfs.push_back(node);
+            for(auto it:adj[node]){
+                if(!vis[it]){
+                    vis[it]=1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+    }
+};
